dc4e8b2c5306fefbbef4d889f6a6a522f10da321
Marcos Scarpim <marcos.scarpim@gmail.com>
2022-07-29 12:53:02 -0300

    Adicionando projeto para aula 03 sem resolução

---
+++ Aula_03/app/src/main/java/br/org/venturus/kotlinjava/java/User.java
@@ -0,0 +1,33 @@
+package br.org.venturus.kotlinjava.java;
+
+import androidx.annotation.Nullable;
+
+public class User {
+
+    @Nullable
+    private String firstName;
+    @Nullable
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
