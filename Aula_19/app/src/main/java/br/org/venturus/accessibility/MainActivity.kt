7e4bedda1a69edcc0472801da90381fd7bf26e37
Marcos Scarpim <marcos.scarpim@gmail.com>
2022-10-04 13:11:12 -0300

    Adicionando código para o Desafio de Acessibilidade
    Adicionando código para ser usado no Desafio de Acessibilidade da
    Aula 19.

---
+++ Aula_19/app/src/main/java/br/org/venturus/accessibility/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.org.venturus.accessibility
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
